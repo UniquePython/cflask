03afebd6b3b02503f655118744bc64f7678f1e67
Ananyo Bhattacharya <ananyobhattacharya@gmail.com>
2026-04-19 16:31:27 +0530

    accept a client and print a message, then close client

--- src/main.c
+++ src/main.c
@@ -1,6 +1,52 @@
 #include <stdio.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
 
-int main(void) {
-    printf("cflask\n");
+#define PORT 8080
+#define BACKLOG 5
+
+int main()
+{
+    int server_fd;
+    struct sockaddr_in server_addr;
+
+    server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_fd < 0)
+    {
+        perror("socket");
+        return 1;
+    }
+
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(PORT);
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+
+    if (bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
+    {
+        perror("bind");
+        return 1;
+    }
+
+    if (listen(server_fd, BACKLOG) < 0)
+    {
+        perror("listen");
+        return 1;
+    }
+
+    printf("Listening on port %d...\n", PORT);
+
+    int client_fd;
+    client_fd = accept(server_fd, NULL, NULL);
+    if (client_fd < 0)
+    {
+        perror("accept");
+        return 1;
+    }
+
+    printf("Client connected!\n");
+
+    close(client_fd);
+    close(server_fd);
     return 0;
 }
